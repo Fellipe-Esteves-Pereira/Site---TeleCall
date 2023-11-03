4efca70347883e4031eee114c9a460f2eb43056f
Fellipe Esteves Pereira <144180761+Fellipe-Esteves-Pereira@users.noreply.github.com>
2023-11-03 19:46:12 -0300

    Delete script directory

--- script/modal.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-  var modal = document.getElementById("Modal");
-  modal.style.display = "block";
-}
-
-function closeModal() {
-  var modal = document.getElementById("Modal");
-  modal.style.display = "none";
-}
